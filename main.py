0293cbbe2dddf497b2e5849a8e607f43b4f60e66
seanpdonovan <seandonbu@gmail.com>
2017-09-13 16:01:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import urllib.request
 import xml.etree.ElementTree as ET
+import time
 
 #user_agent needed or else forbidden request error
 user_agent = 'Mozilla'
@@ -12,8 +13,17 @@ data = response.read()
 
 root = ET.fromstring(data)
 
-#print the names of the currencies
-for child in root:
-	symbol = child.get('Symbol')
-	bid = child.find('Bid').text
-	print(symbol, bid)
+#get the currency and target rate
+Currency = input("Enter the currency to check: ")
+TargetRate = input("Enter the target rate: ")
+CurrentRate = 0
+
+#main loop
+while (CurrentRate != TargetRate):
+	time.sleep(5)
+	for child in root:
+		symbol = child.get("Symbol")
+		bid = child.find("Bid").text
+		if (symbol == Currency):
+			CurrentRate = bid
+			print("Current rate is: " + CurrentRate)
